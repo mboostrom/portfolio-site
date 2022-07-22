1629008c86dd6ca6ec87418ecb834e9549cfe4a0
mboostrom <maxboostrom@gmail.com>
2022-07-22 15:18:52 -0400

    fully styled page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { hot } from "react-hot-loader/root";
+import React, { useState, useEffect } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { hot } from "react-hot-loader/root"
 
-import "../assets/scss/main.scss";
+import "../assets/scss/main.scss"
 
-import TopBar from "./layout/TopBar";
-import HomePage from "./HomePage";
+import TopBar from "./layout/TopBar"
+import HomePage from "./HomePage"
 
 const App = (props) => {
-
   return (
     <Router>
-      <TopBar  />
+      <TopBar />
       <Switch>
         <Route exact path="/">
           <HomePage />
         </Route>
       </Switch>
     </Router>
-  );
-};
+  )
+}
 
-export default hot(App);
+export default hot(App)
---
+++ client/src/components/ReviewButton.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ReviewButton = (props) => {
+  return (
+    <div className="review-button">
+      <a href="#">Review the project</a>
+    </div>
+  )
+}
+
+export default ReviewButton
